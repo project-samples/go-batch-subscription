56cfd462bfd45f4f4093eaf1322fd69a7061f6fe
vatuan <tuanvu95vn@gmail.com>
2021-05-04 17:01:50 +0700

    Apply new version of core-go

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"reflect"
 
+	dyn "github.com/core-go/dynamodb"
 	"github.com/core-go/health"
-	mgo "github.com/core-go/mongo"
 	"github.com/core-go/mq"
 	"github.com/core-go/mq/log"
 	"github.com/core-go/mq/sarama"
@@ -22,9 +22,9 @@ type ApplicationContext struct {
 
 func NewApp(ctx context.Context, root Root) (*ApplicationContext, error) {
 	log.Initialize(root.Log)
-	db, er1 := mgo.SetupMongo(ctx, root.Mongo)
+	db, er1 := dyn.Connect(root.Dynamodb)
 	if er1 != nil {
-		log.Error(ctx, "Cannot connect to MongoDB. Error: "+er1.Error())
+		log.Error(ctx, "Cannot connect to Dynamodb: Error: "+er1.Error())
 		return nil, er1
 	}
 
@@ -41,10 +41,10 @@ func NewApp(ctx context.Context, root Root) (*ApplicationContext, error) {
 	}
 
 	userType := reflect.TypeOf(User{})
-	batchWriter := mgo.NewBatchInserter(db, "users")
+	batchWriter := dyn.NewBatchInserter(db, "users")
 	batchHandler := mq.NewBatchHandler(userType, batchWriter.Write, logError, logInfo)
 
-	mongoChecker := mgo.NewHealthChecker(db)
+	dynamodbChecker := dyn.NewHealthChecker(db)
 	receiverChecker := kafka.NewKafkaHealthChecker(root.Reader.Brokers, "kafka_reader")
 	var healthHandler *health.HealthHandler
 	var batchWorker mq.BatchWorker
@@ -58,10 +58,10 @@ func NewApp(ctx context.Context, root Root) (*ApplicationContext, error) {
 		retryService := mq.NewRetryService(sender.Write, logError, logInfo)
 		batchWorker = mq.NewDefaultBatchWorker(root.BatchWorkerConfig, batchHandler.Handle, retryService.Retry, logError, logInfo)
 		senderChecker := kafka.NewKafkaHealthChecker(root.Writer.Brokers, "kafka_writer")
-		healthHandler = health.NewHealthHandler(mongoChecker, receiverChecker, senderChecker)
+		healthHandler = health.NewHealthHandler(dynamodbChecker, receiverChecker, senderChecker)
 	} else {
 		batchWorker = mq.NewDefaultBatchWorker(root.BatchWorkerConfig, batchHandler.Handle, nil, logError, logInfo)
-		healthHandler = health.NewHealthHandler(mongoChecker, receiverChecker)
+		healthHandler = health.NewHealthHandler(dynamodbChecker, receiverChecker)
 	}
 	checker := v.NewErrorChecker(NewUserValidator().Validate)
 	validator := mq.NewValidator(userType, checker.Check)
